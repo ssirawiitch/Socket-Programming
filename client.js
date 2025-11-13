49a5818c0e52961b01a0846b10bffd377e133a0b
ssirawiitch <sirawitchchairuangsirikul@gmail.com>
2025-11-13 11:38:24 +0700

    fix link

--- client.js
+++ client.js
@@ -8,7 +8,7 @@ function connect() {
   username = document.getElementById("username").value.trim();
   if (!username) return alert("Please enter your name!");
 
-  socket = new WebSocket("wss://https://socket-programming-2.onrender.com/");
+  socket = new WebSocket("wss://socket-programming-2.onrender.com/ws");
   socket.onopen = () => socket.send(username);
 
   socket.onmessage = (event) => {
